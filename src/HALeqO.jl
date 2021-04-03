fadc228cc8bdd0eac7cd136361be848b706e7ee1
Alberto De Marchi <albertodm92@libero.it>
2021-04-03 12:29:22 +0200

    improved mem management, add comments, minor printing changes

--- src/HALeqO.jl
+++ src/HALeqO.jl
@@ -16,9 +16,9 @@ function normsq(v)
 end
 
 """
-    merit(x, y, xhat, yhat, μ, fx, cx)
+    merit(x, y, yhat, μ, fx, cx)
 """
-function merit(x, y, xhat, yhat, μ, fx, cx)
+function merit(x, y, yhat, μ, fx, cx)
     return fx + 0.25 * μ * normsq(y) + normsq(cx + μ .* (yhat - 0.5 * y)) / μ
 end
 
@@ -38,7 +38,7 @@ function haleqo(
     start_time = time()
     nx, ny = nlp.meta.nvar, nlp.meta.ncon
     T = eltype(x)
-    
+
     if has_bounds(nlp) || inequality_constrained(nlp)
         error("Problem has inequalities, can't solve it")
     end
@@ -54,49 +54,54 @@ function haleqo(
     iter = 0
     status = :unknown
     fx = obj(nlp, x)
-    dlagr = grad(nlp, x) + jtprod(nlp, x, y)
     cx = cons(nlp, x)
-    xhat = copy(x)
     yhat = copy(y)
     σhat = copy(σ)
     subres = zeros(T, nx + ny)
-    subres[1:nx] .= dlagr
+    subres[1:nx] .= grad(nlp, x) + jtprod(nlp, x, y)
     subres[nx+1:nx+ny] .= cx
-    optimality = norm(dlagr, Inf)
+    optimality = norm(subres[1:nx], Inf)
     cviolation = norm(cx, Inf)
+    cviol__old = cviolation
     residy = cviolation
     converged = optimality ≤ tol && cviolation ≤ tol
     tired = iter ≥ max_iter
     dir = zeros(T, nx + ny)
-    dmerit = zeros(T, nx + ny)
     xold = zeros(T, nx)
     yold = zeros(T, ny)
 
     @info log_header(
-        [:iter, :fx, :optim, :cviol, :σ, :μ, :resx, :resy],
-        [Int, T, T, T, T, T, T, T],
+        [:iter, :fx, :cviol, :optim, :σ, :μ, :resy],
+        [Int, T, T, T, T, T, T],
         hdr_override = Dict(:fx => "f(x)", :optim => "‖∇L‖", :cviol => "‖c(x)‖"),
     )
-    @info log_row(Any[iter, fx, optimality, cviolation])
+    @info log_row(Any[iter, fx, cviolation, optimality])
 
     while !(converged || tired)
 
         if optimality ≤ tol && residy ≤ tol
 
-            if !(cviolation ≤ θ * norm(cons(nlp, xhat), Inf))
+            if cviolation > θ * cviol__old
                 μ *= κμminus
             end
 
-            xhat .= x
+            @info log_row(Any[iter, fx, cviolation, optimality, σ, μ])
             yhat .= y
-            @info log_row(Any[iter, fx, optimality, cviolation, σ, μ])
+            subres[nx+1:nx+ny] .= cx
+            cviol__old = cviolation
+            residy = cviolation
         else
-            @info log_row(Any[iter, fx, optimality, cviolation, σ, μ, optimality, residy])
+            @info log_row(Any[iter, fx, cviolation, optimality, σ, μ, residy])
         end
 
+        # search direction
+        # compute Newton direction for the regularized sub-problem, possibly with
+        # some perturbation of the Hessian matrix to obtain a descent direction
+        # for the merit function
         H = hess(nlp, x, y)
-        H = H + H' - triu(H)
+        J = jac(nlp, x)
 
+        H = H + H' - triu(H)
         σ = σhat
         HσI = H + UniformScaling(σ)
         Hf, Hd = ldlt(Positive, HσI, Val{true})
@@ -106,44 +111,51 @@ function haleqo(
             Hf, Hd = ldlt(Positive, HσI, Val{true})
         end
 
-        J = jac(nlp, x)
         KKT = [HσI J'; J UniformScaling(-μ)]
-        subres[1:nx] .= dlagr
-        subres[nx+1:nx+ny] .= cx + μ .* (yhat - y)
         LDLT = Ma57(KKT)
         ma57_factorize(LDLT)
-        dir .= ma57_solve(LDLT, -subres)
+        dir .= -subres
+        ma57_solve!(LDLT, dir)
 
-        dmerit[1:nx] .= subres[1:nx] + (2.0 / μ) .* jtprod(nlp, x, subres[nx+1:nx+ny])
-        dmerit[nx+1:nx+ny] .= -subres[nx+1:nx+ny]
-        slope = dot(dir, dmerit)
+        # gradient of merit function
+        # ∇x merit = resx + (2/μ) resy
+        # ∇y merit = - resy
+        xold .= subres[1:nx] + (2.0 / μ) .* jtprod(nlp, x, subres[nx+1:nx+ny])
+        # slope of merit along search direction
+        slope = dot(dir[1:nx], xold) - dot(dir[nx+1:nx+ny], subres[nx+1:nx+ny])
         slope < 0.0 || @warn "nonnegative slope"
+
+        # line-search
+        # backtracking line-search along the primal-dual augmented Lagrangian
+        # merit function with Armijo sufficient decrease condition
         xold .= x
         yold .= y
-        mold = merit(x, y, xhat, yhat, μ, fx, cx)
-        τ = 1.0
+        mold = merit(x, y, yhat, μ, fx, cx)
+        τ = one(T)
         x .+= dir[1:nx]
         y .+= dir[nx+1:nx+ny]
         fx = obj(nlp, x)
         cons!(nlp, x, cx)
-        m = merit(x, y, xhat, yhat, μ, fx, cx)
-        while !(m ≤ mold + η * τ * slope)
+        m = merit(x, y, yhat, μ, fx, cx)
+        while m > mold + η * τ * slope
             τ *= β
             x .= xold + τ .* dir[1:nx]
             y .= yold + τ .* dir[nx+1:nx+ny]
             fx = obj(nlp, x)
             cons!(nlp, x, cx)
-            m = merit(x, y, xhat, yhat, μ, fx, cx)
+            m = merit(x, y, yhat, μ, fx, cx)
         end
 
-        dlagr .= grad(nlp, x) + jtprod(nlp, x, y)
-        optimality = norm(dlagr, Inf)
+        subres[1:nx] .= grad(nlp, x) + jtprod(nlp, x, y)
+        subres[nx+1:nx+ny] .= cx + μ .* (yhat - y)
+
+        optimality = norm(subres[1:nx], Inf)
+        residy = norm(subres[nx+1:nx+ny], Inf)
         cviolation = norm(cx, Inf)
+
         iter += 1
         converged = optimality ≤ tol && cviolation ≤ tol
         tired = iter ≥ max_iter
-        subres[nx+1:nx+ny] .= cx + μ .* (yhat - y)
-        residy = norm(subres[nx+1:nx+ny], Inf)
     end
 
     eltime = time() - start_time
